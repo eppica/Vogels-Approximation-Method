9df3af431a4fed55116e315d06cb8fdc94975d8b
Pedro Henrique Ferreira <pedrohfcd@hotmail.com>
2021-03-21 17:40:14 -0300

    Create generic_problem

---
+++ problems/generic_problem.py
@@ -0,0 +1,17 @@
+origin = ['1', ',2', '3']
+
+destination = ['1', ',2', '3', '4']
+
+matrix = [
+    [2, 3, 11, 7],
+    [1, 0, 6, 1],
+    [5, 8, 15, 9]
+]
+
+need = [7, 5, 3, 2]
+
+availability = [6, 1, 10]
+
+origin_penalty = []
+
+destination_penalty = []
